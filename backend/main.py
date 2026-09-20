fefdfbb4a7e130d53a7d3cd1cb04e8add466bc7c
wcy-0312 <jimmyking0312@gmail.com>
2026-09-20 18:00:17 +0800

    Restrict CORS origins

--- backend/main.py
+++ backend/main.py
@@ -252,7 +252,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://wcy-0312.github.io",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
